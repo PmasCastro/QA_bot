af64cf61e311ea558142effc95597a1c664184b9
PmasCastro <pmas.castro@gmail.com>
2025-04-14 15:39:21 +0100

    added tool to get real time weather forecast, currently it fetches info based on lat/lon so not 100% accurate when given location, might add some geocoding api

--- main.py
+++ main.py
@@ -2,37 +2,85 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 import gradio as gr
-import fitz  # PyMuPDF
+import json
+import requests
 
 load_dotenv(override=True)
 
 openai_api_key = os.getenv('OPENAI_API_KEY')
 openai = OpenAI()
 
-MODEL = "gpt-4o-mini"
+gpt_model = "gpt-4o-mini"
+ollama_model = "llama3.1"
 
-system_message = "You are a helpful Q&A bot."
-system_message += "You specialize in Philosophy."
-system_message += "You only answer questions related to philosophy"
+system_message = "You are a helpful assistant"
 
-# Load PDF text once at startup
-def extract_text_from_pdf(pdf_path):
-    doc = fitz.open(pdf_path)
-    text = ""
-    for page in doc:
-        text += page.get_text()
-    return text
+tools = [{
+    "type": "function",
+    "function": {
+        "name": "get_weather",
+        "description": "Get current temperature for a given coordinates.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "latitude": {
+                    "type": "number",
+                    "description": "Latitude of the location"
+                },
+                "longitude": {
+                    "type": "number",
+                    "description": "Longitude of the location"
+                }
+            },
+            "required": ["latitude", "longitude"],
+            "additionalProperties": False
+        }
+    }
+}]
 
-pdf_text = extract_text_from_pdf("phildb.pdf")
+def get_weather(latitude, longitude):
+    response = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m")
+    data = response.json()
+    return data['current']['temperature_2m']
+
 
 def chat(message, history):
     messages = [{"role": "system", "content": system_message}] + history + [{"role": "user", "content": message}]
+    response = openai.chat.completions.create(model=gpt_model, messages=messages, tools=tools)
+    
+    if response.choices[0].finish_reason=="tool_calls": #checks if model needs to use tool
+        message = response.choices[0].message
+        response = handle_tool_call(message) #runs get_weather function
+        messages.append(message)
+        messages.append(response)
+        response = openai.chat.completions.create(model=gpt_model, messages=messages)
+    
+    return response.choices[0].message.content
 
-    stream = openai.chat.completions.create(model=MODEL, messages=messages, stream=True)
+def handle_tool_call(message):
+    tool_call = message.tool_calls[0]
+    args = json.loads(tool_call.function.arguments) #gpt sends arguments as JSON string, not a dict, this line converts it so you can work with it
+    result = get_weather(args["latitude"], args["longitude"])
+    response = {
+        "role": "tool",
+        "content": json.dumps({"temperature": result}),
+        "tool_call_id": tool_call.id
+    }
+    return response
 
-    response = ""
-    for chunk in stream:
-        response += chunk.choices[0].delta.content or ''
-        yield response
 
 gr.ChatInterface(fn=chat, type="messages").launch(inbrowser=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ pdf_reader.py
@@ -0,0 +1,10 @@
+
+
+# def parse_pdf(pdf_path):
+#     doc = fitz.open(pdf_path)
+#     text = ""
+#     for page in doc:
+#         text += page.get_text()
+#     return text
+
+# pdf_text = parse_pdf("phildb.pdf")
